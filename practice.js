cde6caa55feb82d02b85efc9b4fc0c6ae37580cf
shadtek <spencer.shattuck@gmail.com>
2015-04-11 17:23:58 -0600

    added 4 employees to the devMountainEmployees array

--- practice.js
+++ practice.js
@@ -258,18 +258,20 @@ var colt = {
     name: 'Colt',
     position: 'Everything really',
     spiritAnimal: 'Young Male Horse'
-}
+};
 
 /*Above you're given an empty array with four objects. Fill the devMountainEmployees
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+//Code Here
+devMountainEmployees.push(tyler, cahlan, ryan, colt);
+console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absense to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+//Code Here
 
 
 
